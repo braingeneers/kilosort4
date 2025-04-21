80d8638ed24daa2d8ff214e1914ff524440b34fd
DailyDreaming <lblauvel@ucsc.edu>
2025-04-21 14:02:16 -0700

    Organize code in run.py better.

--- run.py
+++ run.py
@@ -15,8 +15,14 @@ from spikeinterface.extractors import read_nwb_recording, read_maxwell
 this_dir = os.path.dirname(__file__)
 output_dir = os.path.join(this_dir, 'output')
 
+s3_client = boto3.client('s3', endpoint_url='https://s3-west.nrp-nautilus.io')
+braingeneers_bucket = 'braingeneers'
+kilosort_version = 'kilosort4'
 
-def zip_files(dir: str, zip_filename: str):
+
+def zip_files(dir: str, zip_filename: str) -> None:
+    """Given a directory with multiple files, this will zip them into one file."""
+    print(f'Zipping files in {dir} to: {zip_filename}')
     with zipfile.ZipFile(zip_filename, 'w') as z:
         for root, dirs, files in os.walk(dir, topdown=False):
             for f in files:
@@ -24,7 +30,38 @@ def zip_files(dir: str, zip_filename: str):
                 z.write(f, os.path.basename(f))
 
 
-def mock_run_sorter(input_filepath: str):
+def upload_local_to_s3(src: str, dst: str) -> None:
+    """Given a local filepath, this will upload it to an s3 URI."""
+    if not dst.startswith(f's3://{braingeneers_bucket}/ephys/'):
+        print(f'URI is unexpected and non-canonical ({dst})!  Skipping upload to s3.', file=sys.stderr)
+
+    uuid = dst[len(f's3://{braingeneers_bucket}/ephys/'):].split('/', maxsplit=1)[0]
+    dst_s3_key = f'ephys/{uuid}/derived/{kilosort_version}/{src}'
+    print(f'Uploading {src} to {dst} with boto3 version: {boto3.__version__}')
+    s3_client.upload_file(src, braingeneers_bucket, dst[len(f's3://{braingeneers_bucket}/'):])
+
+
+def download_s3_to_local(src: str, dst: str) -> None:
+    """Given an s3 URI, this will download it locally."""
+    if not src.startswith('s3://'):
+        raise RuntimeError(f'Input filepath must start with s3:// !  {src}')
+
+    print(f'Downloading {src} to {dst} with boto3 version: {boto3.__version__}')
+    bucket, key = src[len('s3://'):].split('/', maxsplit=1)
+    s3_client.download_file(bucket, key, dst)
+
+
+def s3_destination(src: str, filename: str):
+    """From an input s3 URI, determine the s3 destination URI where kilosort outputs should go."""
+    if not src.startswith(f's3://{braingeneers_bucket}/ephys/'):
+        print(f'URI is unexpected and non-canonical ({src})!  Skipping upload to s3.', file=sys.stderr)
+
+    uuid = src[len(f's3://{braingeneers_bucket}/ephys/'):].split('/', maxsplit=1)[0]
+    return f's3://{braingeneers_bucket}/ephys/{uuid}/derived/{kilosort_version}/{filename}'
+
+
+def mock_run_sorter(input_filepath: str) -> str:
+    """Writes three text files with some content into an output dir."""
     os.makedirs(output_dir, exist_ok=True)
     for file in ['a.npy', 'b.npy', 'c.npy']:
         with open(os.path.join(output_dir, file), 'w') as f:
@@ -32,16 +69,13 @@ def mock_run_sorter(input_filepath: str):
     return input_filepath
 
 
-def main(mock: bool = False):
-    input_filepath: str = sys.argv[1]
-    s3_client = boto3.client('s3', endpoint_url='https://s3-west.nrp-nautilus.io')
-
-    if input_filepath.startswith('s3://'):
-        print(f'Downloading {input_filepath} with boto3 version: {boto3.__version__}')
-        bucket, key = input_filepath[len('s3://'):].split('/', maxsplit=1)
-        s3_client.download_file(bucket, key, f'/tmp/{os.path.basename(input_filepath)}')
-    else:
-        raise RuntimeError(f'Input filepath must start with s3:// !  {input_filepath}')
+def process_with_kilosort(input_filepath: str, mock: bool = False) -> str:
+    """
+    Accepts a local Maxwell h5 file as an input and produces a few dozen (mostly numpy array) files
+    in an output dir.
+    """
+    if mock:
+        return mock_run_sorter(input_filepath)
 
     params = {"acg_threshold": 0.1,
               # algorithm lloyd
@@ -93,30 +127,25 @@ def main(mock: bool = False):
               }
 
     torch.cuda.empty_cache()
-    if mock:
-        multisorting = mock_run_sorter(input_filepath)
-    else:
-        multisorting = ss.run_sorter(
-            'kilosort4',
-            read_maxwell(f'/tmp/{os.path.basename(input_filepath)}'),
-            folder=output_dir,
-            remove_existing_folder=True,
-            verbose=True,
-            # **params
-        )
-    print(multisorting)
+    return ss.run_sorter(
+        kilosort_version,
+        read_maxwell(input_filepath),
+        folder=output_dir,
+        remove_existing_folder=True,
+        verbose=True,
+        # **params
+    )
 
-    zip_filename = f'{os.path.basename(input_filepath)}.ks4.zip'
-    print(f'Zipping files to: {zip_filename}')
+
+def main(mock: bool = False):
+    input_uri: str = sys.argv[1]
+    zip_filename: str = f'{os.path.basename(input_uri)}.ks4.zip'
+    local_filepath: str = f'/tmp/{os.path.basename(input_uri)}'
+
+    download_s3_to_local(src=input_uri, dst=local_filepath)
+    process_with_kilosort(local_filepath, mock=mock)
     zip_files(dir=output_dir, zip_filename=zip_filename)
-
-    if input_filepath.startswith('s3://braingeneers/ephys/'):
-        uuid = input_filepath[len('s3://braingeneers/ephys/'):].split('/', maxsplit=1)[0]
-        output_s3_key = f'ephys/{uuid}/derived/{zip_filename}'
-        print(f'Uploading {zip_filename} to s3://braingeneers/{output_s3_key} with boto3 version: {boto3.__version__}')
-        s3_client.upload_file(zip_filename, 'braingeneers', output_s3_key)
-    else:
-        print(f'Input filepath is unexpected and non-canonical ({input_filepath})!  Skipping upload to s3.', file=sys.stderr)
+    upload_local_to_s3(src=zip_filename, dst=s3_destination(input_uri, zip_filename))
 
 
 if __name__ == '__main__':
